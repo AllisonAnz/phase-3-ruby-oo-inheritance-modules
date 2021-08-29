2b6a8999e398bb7f921daeeb53ab6d8d5273abfb
AllisonAnz <AllisonAnzalone@gmail.com>
2021-08-29 14:01:21 -0500

    Completed Assignment

---
+++ Modules.rb
@@ -0,0 +1,18 @@
+# Modules 
+# Previously we discussed the concept of inheritance 
+# We learned that through subclasses(a certain class under superclass), the subclass has 
+# access to all of the methods of its parent class 
+
+# Think of an example, one that is less hierarchical 
+# Like writing an app that modules the envirionment of dance performances 
+# Such an app might have a Dance class 
+# Dancers perform dances 
+
+# Similarly we could imagine little kids going to see the Nutcracker ballet one Chirstmas 
+
+# WE might write a Kid class in which as instance of the class 
+# our little kid has access to all those ballet moves 
+
+# This is where modules come in 
+# Modules allow us to collect and bundle a group of methods and make those methods 
+# available to any number of classes 
---
+++ lib/dance_module.rb
@@ -0,0 +1,20 @@
+# Step 1: Define Our Module 
+#           give it some moves 
+# Step 2: Define our Kid class and tell it to include the capabilities of Dance module
+module Dance 
+    def twirl
+      "I'm twirling!"
+    end
+
+    def jump
+      "Look how high I'm jumping!"
+    end
+
+    def pirouette
+      "I'm doing a pirouette"
+    end
+
+    def take_a_bow
+      "Thank you, thank you. It was a pleasure to dance for you all."
+    end
+end 
